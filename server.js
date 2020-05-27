b97b4705eb0001d63683f807c5956d818bba0077
Michael Schiumo <mschiumo18@gmail.com>
2020-05-27 17:15:04 -0400

    added express validations for user model

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect DB
 connectDB();
 
+// init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.json({ msg: 'Welcome' }));
 
 //define routes
